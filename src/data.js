8b706068afda8905572fcc471cc4df6fda85f2db
Sobayo <sobayotoby@gmail.com>
2023-08-01 18:49:47 +0100

    added new project to portfolio

--- src/data.js
+++ src/data.js
@@ -1,39 +1,45 @@
 export const projectItem = [
   {
     id: 1,
+    img: "https://i.ibb.co/wwZmSnT/scissor-img.png",
+    link: "https://scissor-shortener.netlify.app/",
+    desc: "URL Shortener",
+  },
+  {
+    id: 2,
     img: "https://i.ibb.co/K7GD1sg/projectshot1.png",
     link: "https://sobayo-bootstrap-portfolio.netlify.app/",
-    desc: "Bootstrap portfolio"
+    desc: "Bootstrap portfolio",
   },
-  
+
   {
     id: 3,
     img: "https://i.ibb.co/njKN719/projectshot3.png",
     link: "https://sobayo-open-source1.netlify.app/",
-    desc: "Open source"
-  },
-  {
-    id: 2,
-    img: "https://i.ibb.co/s50BrC3/projectshot2.png",
-    link: "https://sobayo-restaurant-website.netlify.app/",
-    desc: "Restaurant website"
+    desc: "Open source",
   },
   {
     id: 4,
-    img: "https://i.ibb.co/5FZ1G4d/projectshot4.png",
-    link: "https://sobayo-photography-site.netlify.app/",
-    desc: "Photography website"
+    img: "https://i.ibb.co/s50BrC3/projectshot2.png",
+    link: "https://sobayo-restaurant-website.netlify.app/",
+    desc: "Restaurant website",
   },
   {
     id: 5,
-    img: "https://i.ibb.co/3m8Pmk2/projectshot5.png",
-    link: "https://sobayo-parallax-site.netlify.app/",
-    desc: "Parallax website"
+    img: "https://i.ibb.co/5FZ1G4d/projectshot4.png",
+    link: "https://sobayo-photography-site.netlify.app/",
+    desc: "Photography website",
   },
   {
     id: 6,
+    img: "https://i.ibb.co/3m8Pmk2/projectshot5.png",
+    link: "https://sobayo-parallax-site.netlify.app/",
+    desc: "Parallax website",
+  },
+  {
+    id: 7,
     img: "https://i.ibb.co/wp3z9bp/projectshot6.png",
     link: "https://sobayo-music-store.netlify.app/",
-    desc: "Music store website"
+    desc: "Music store website",
   },
 ];
